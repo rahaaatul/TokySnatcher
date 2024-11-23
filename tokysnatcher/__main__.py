f5cbd9b7ed1b7d89882332cebc562360da71cd14
Rahatul Ghazi <86149861+rahaaatul@users.noreply.github.com>
2024-11-23 19:05:10 +0600

    refactor: improve main script structure and error handling
    - Renamed `cls` function to `clear_terminal` for better readability (unused).
    - Moved `choices` inside the `main` function to limit its scope.
    - Added `sys` import for handling script exit.
    - Improved error handling by checking if `selected_action` is `None` and exiting gracefully.
    - Wrapped `main` function call in a `try` block to handle `KeyboardInterrupt` and exit gracefully.
    - Renamed variables for clarity: `options` to `actions` and `res` to `result`.

--- tokysnatcher/__main__.py
+++ tokysnatcher/__main__.py
@@ -1,36 +1,45 @@
 import argparse
 from os import name, system
-
 import questionary
+import sys
 
 from .chapters import get_chapters
 from .download import get_input
 from .search import search_book
 
 
-def cls() -> None:
-    """Clear terminal."""
-    return system("cls" if name == "nt" else "clear")  # noqa: S605
-
-
-choices = ["Search book", "Download from URL", "Exit"]
+def clear_terminal() -> None:
+    """Clear the terminal screen."""
+    system("cls" if name == "nt" else "clear")  # noqa: S605
 
 
 def main() -> None:
-    """Get argument from CLI."""
-    parser = argparse.ArgumentParser(description="TokySnatcher - Download Audiobooks from TokyBook")
-    parser.add_argument("-d", "--directory", type=str, default=None, help="Custom download directory")
+    """Get argument from CLI and execute the selected action."""
+    parser = argparse.ArgumentParser(
+        description="TokySnatcher - Download Audiobooks from TokyBook"
+    )
+    parser.add_argument(
+        "-d", "--directory", type=str, default=None, help="Custom download directory"
+    )
     args = parser.parse_args()
 
+    choices = ["Search book", "Download from URL", "Exit"]
     selected_action = questionary.select("Choose action:", choices=choices).ask()
 
-    options = [search_book, get_input, exit]
-    res = options[choices.index(selected_action)]()
+    if selected_action is None:
+        sys.exit(0)
 
-    if res:
-        # cls()
-        get_chapters(res, args.directory)
+    actions = [search_book, get_input, sys.exit]
+    action = actions[choices.index(selected_action)]
+    result = action()
+
+    if result:
+        # clear_terminal()
+        get_chapters(result, args.directory)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
